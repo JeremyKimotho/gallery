a5bf030f9f05d7ccebcadb5a34a1ff6e3e09d79a
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-12-09 11:12:08 +0300

    the admin superuser has been created

--- images/views.py
+++ images/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, render_to_response, redirect
 import datetime as dt
-from .models import Image
+from .models import Image, Location, Category
 from django.http import HttpResponse, Http404
 
 def default(request):
